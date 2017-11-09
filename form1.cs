36407356341fecf20502d12785fb6dbde6d14e33
Монтажник 2 <montager2@ntrk21.ru>
2017-11-09 16:03:48 +0300

    second

---
+++ form1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloApp
+{
+	public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private System.ComponentModel.IContainer components = null;
+ 
+ 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Text = "Привет мир!";
+            this.ResumeLayout(false);
+ 
+        }
+    }
+}
---
+++ start.bat
@@ -0,0 +1,5 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe "e:\Volodia WORK\Program\*.cs"
+
+main.exe
+TIMEOUT /t 20 /nobreak
+:: pause
